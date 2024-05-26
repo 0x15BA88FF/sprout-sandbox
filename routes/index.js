9f0f50c29432923c38aa698e232f8b5797228e49
pascall de creator <pascall.de.creator@gmail.com>
2024-05-26 03:21:39 -0700

    Modify routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', { products: [
+        {
+            title: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum laborum explicabo minima nisi libero quidem!",
+            rating: 3,
+            currency: 'GHS',
+            price: 59.99,
+            quantity: 'pcs',
+            isInCart: false,
+        }
+    ]
+    });
 });
 
 module.exports = router;
